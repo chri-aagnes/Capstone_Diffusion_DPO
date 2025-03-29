8509a8bb2550b54cad05c9bf1319edaa45008087
chri-aagnes <chri.aagnes@gmail.com>
2025-03-29 16:32:42 -0400

    fixed reqs and changed to subset of dataset

--- launchers/sd15.sh
+++ launchers/sd15.sh
@@ -1,5 +1,5 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
-export DATASET_NAME="yuvalkirstain/pickapic_v2"
+export DATASET_NAME="yanher/pickapic_subset" # alyssa's huggingface subset
 
 # Effective BS will be (N_GPU * train_batch_size * gradient_accumulation_steps)
 # Paper used 2048. Training takes ~24 hours / 2000 steps
@@ -9,11 +9,11 @@ accelerate launch train.py \
   --dataset_name=$DATASET_NAME \
   --train_batch_size=1 \
   --dataloader_num_workers=16 \
-  --gradient_accumulation_steps=128 \
-  --max_train_steps=2000 \
-  --lr_scheduler="constant_with_warmup" --lr_warmup_steps=500 \
+  --gradient_accumulation_steps=16 \
+  --max_train_steps=32 \
+  --lr_scheduler="constant_with_warmup" --lr_warmup_steps=4 \
   --learning_rate=1e-8 --scale_lr \
-  --cache_dir="/export/share/datasets/vision_language/pick_a_pic_v2/" \
-  --checkpointing_steps 500 \
+  --cache_dir="/n/home00/caagnes/cache" \
+  --checkpointing_steps 8 \
   --beta_dpo 5000 \
    --output_dir="tmp-sd15"
